1ed69e368c11d1f20b12ccbbf5b8a64be713aca3
Sterling Hanenkamp <sterling@hanenkamp.com>
2023-03-05 00:00:27 -0600

    godoc and fix config layer scopes

--- config/config.go
+++ config/config.go
@@ -12,54 +12,135 @@ import (
 	"github.com/zostay/zedpm/storage"
 )
 
+// Config is the master configuration as we use it in the application. The
+// configuration format is in HCL. The actual HCL definition are with the Raw*
+// structures, which are converted into these structures to handle the
+// conversion of generic JSON-style objects used for Properties into storage.KV
+// objects as working with JSON-style objects in HCL is ugly without some
+// conversion like this.
 type Config struct {
+	// Properties are the global properties that are used as the value if not
+	// overridden by any other configuration section.
 	Properties storage.KV
 
-	Goals   []GoalConfig
+	// Goals is the configuration to apply to each goal.
+	Goals []GoalConfig
+
+	// Plugins is the configuration to apply to each plugin.
 	Plugins []PluginConfig
 }
 
 type GoalConfig struct {
+	// Name is the name of the goal being configured.
 	Name string
 
-	EnabledPlugins  []string
+	// EnabledPlugins creates an allow list of plugins to use when executing
+	// this goal. If an enable list is provided, then only plugins on this list
+	// will be executed.
+	//
+	// TODO Implement EnabledPlugins functionality for goals.
+	EnabledPlugins []string
+
+	// DisabledPlugins creates a block list of plugins to disable when executing
+	// this goal. If a disabled list is provided, then the listed plugins will
+	// not be executed when running this goal, even if they are listed in the
+	// EnabledPlugins list.
+	//
+	// TODO Implement DisabledPlugins functionality for goals.
 	DisabledPlugins []string
 
+	// Properties provides settings that override globals when executing this
+	// goal or one of its sub-tasks.
 	Properties storage.KV
 
-	Tasks   []TaskConfig
+	// Tasks provides configuration of sub-tasks of this goal.
+	Tasks []TaskConfig
+
+	// Targets provides configuration of targets as applies ot this goal.
 	Targets []TargetConfig
 }
 
+// PluginConfig holds the configuration to use for a particular plugin.
 type PluginConfig struct {
-	Name    string
+	// Name is the name to give the plugin.
+	Name string
+
+	// Command is the command to execute to run the plugin.
 	Command string
 
+	// Properties provides settings that overwrite globals when executing this
+	// plugin.
 	Properties storage.KV
 }
 
+// TaskConfig is the configuration for a sub-task, which is always nested within
+// a goal configuration. This configuration will be employed while running this
+// sub-task regardless of how executed from the command-line.
+//
+// TODO The sub-sub-task configuration here seems inconsistent and needs a look.
 type TaskConfig struct {
-	Name    string
-	SubTask string
-
-	EnabledPlugins  []string
-	DisabledPlugins []string
-
-	Properties storage.KV
-
-	Targets []TargetConfig
-	Tasks   []TaskConfig
-}
-
-type TargetConfig struct {
+	// Name is the name of the sub-task.
 	Name string
 
-	EnabledPlugins  []string
+	// SubTask is the name of the sub-sub-task (and may be empty).
+	SubTask string
+
+	// EnabledPlugins creates an allow list of plugins to use when executing
+	// this sub-task. If an enable list is provided, then only plugins on this
+	// list will be executed.
+	//
+	// TODO Implement EnabledPlugins functionality for tasks.
+	EnabledPlugins []string
+
+	// DisabledPlugins creates a block list of plugins to disable when executing
+	// this sub-task. If a disabled list is provided, then the listed plugins
+	// will not be executed when running this sub-task, even if they are listed
+	// in the EnabledPlugins list.
+	//
+	// TODO Implement DisabledPlugins functionality for tasks.
 	DisabledPlugins []string
 
+	// Properties are the settings used to override globals and goal settings
+	// when executing this sub-task.
+	Properties storage.KV
+
+	// Targets is configuration that should apply just to this sub-task.
+	Targets []TargetConfig
+
+	// Tasks is nested sub-tasks.
+	Tasks []TaskConfig
+}
+
+// TargetConfig is the configuration of a target, which allows for multiple
+// configurations for each goal in case you need a different configuration per
+// environment or per output binary or whatever.
+type TargetConfig struct {
+	// Name is the name to give the target.
+	Name string
+
+	// EnabledPlugins creates an allow list of plugins to use when executing
+	// this target. If an enable list is provided, then only plugins on this
+	// list will be executed.
+	//
+	// TODO Implement EnabledPlugins functionality for targets.
+	EnabledPlugins []string
+
+	// DisabledPlugins creates a block list of plugins to disable when executing
+	// this target. If a disabled list is provided, then the listed plugins
+	// will not be executed when running this sub-task, even if they are listed
+	// in the EnabledPlugins list.
+	//
+	// TODO Implement DisabledPlugins functionality for targets.
+	DisabledPlugins []string
+
+	// Properties are the settings that will override those of the global
+	// settings or the parent goal or task.
 	Properties storage.KV
 }
 
+// Load will load the HCL configuration from the given io.Reader, using the
+// given filename as the one passed to the HCL library, which it uses to help
+// generate useful error messages.
 func Load(filename string, in io.Reader) (*Config, error) {
 	var raw RawConfig
 	fileBytes, err := io.ReadAll(in)
@@ -80,6 +161,8 @@ func Load(filename string, in io.Reader) (*Config, error) {
 	return decodeRawConfig(&raw)
 }
 
+// GoalAndTaskNames splits a string of the form /goal/task/subtask and returns
+// it as "goal" and []string{"task", "subtask"}.
 func GoalAndTaskNames(taskPath string) (string, []string) {
 	taskPath = path.Clean(taskPath)
 	if taskPath == "" || taskPath == "/" {
@@ -98,17 +181,22 @@ func GoalAndTaskNames(taskPath string) (string, []string) {
 	return taskParts[0], taskParts[1:]
 }
 
+// GetGoalFromPath returns the goal configuration for the given task path.
 func (c *Config) GetGoalFromPath(taskPath string) *GoalConfig {
 	goalName, _ := GoalAndTaskNames(taskPath)
 	return c.GetGoal(goalName)
 }
 
+// syntheticGoal is used to generate an empty GoalConfig in cases where we need
+// such a thing.
 func syntheticGoal(name string) *GoalConfig {
 	return &GoalConfig{
 		Name: name,
 	}
 }
 
+// GetGoalAndTasks returns the goal and configuration of applicable sub-tasks
+// for the given task path.
 func (c *Config) GetGoalAndTasks(taskPath string) (*GoalConfig, []*TaskConfig) {
 	goalName, taskNames := GoalAndTaskNames(taskPath)
 
@@ -134,6 +222,7 @@ TaskLoop:
 	return goal, tasks
 }
 
+// GetGoal returns the GoalConfig for the given goal name.
 func (c *Config) GetGoal(goalName string) *GoalConfig {
 	for i := range c.Goals {
 		if c.Goals[i].Name == goalName {
@@ -143,6 +232,7 @@ func (c *Config) GetGoal(goalName string) *GoalConfig {
 	return nil
 }
 
+// GetPlugin returns the PluginConfig for the given plugin name.
 func (c *Config) GetPlugin(pluginName string) *PluginConfig {
 	for i := range c.Plugins {
 		if c.Plugins[i].Name == pluginName {
@@ -152,15 +242,27 @@ func (c *Config) GetPlugin(pluginName string) *PluginConfig {
 	return nil
 }
 
-func (c *Config) GetPluginByCommand(pluginCommand string) *PluginConfig {
-	for i := range c.Plugins {
-		if c.Plugins[i].Command == pluginCommand {
-			return &c.Plugins[i]
-		}
-	}
-	return nil
-}
-
+// ToKV builds and returns a storage.KVLayer containing the configuration layers
+// matching the given taskPath, targetName, and pluginName in proper order
+// (i.e., so that scope overrides happen correctly). The given properties store
+// will be the top-most layer.
+//
+// The scopes override each other in the following order, with the first
+// mentioned item overriding everything below it:
+//
+// 1. Property Settings (from the given properties argument)
+//
+// 2. Target Settings on Task
+//
+// 3. Task Settings
+//
+// 4. Target Settings on Goal
+//
+// 5. Goal Settings
+//
+// 6. Plugin Settings
+//
+// 7. Global Settings
 func (c *Config) ToKV(
 	properties storage.KV,
 	taskPath,
@@ -184,27 +286,7 @@ func (c *Config) ToKV(
 	layers := make([]storage.KV, 0, (len(tasks)+2)*2+1)
 	layers = append(layers, properties)
 
-	layers = append(layers, c.Properties)
-	if plugin != nil {
-		layers = append(layers, plugin.Properties)
-	}
-
-	if goal != nil {
-		layers = append(layers, goal.Properties)
-
-		var target *TargetConfig
-		if targetName != "" {
-			target = goal.GetTarget(targetName)
-		}
-
-		if target != nil {
-			layers = append(layers, target.Properties)
-		}
-	}
-
 	for _, task := range tasks {
-		layers = append(layers, task.Properties)
-
 		var target *TargetConfig
 		if targetName != "" {
 			target = task.GetTarget(targetName)
@@ -213,11 +295,34 @@ func (c *Config) ToKV(
 		if target != nil {
 			layers = append(layers, target.Properties)
 		}
+
+		layers = append(layers, task.Properties)
+
 	}
+
+	if goal != nil {
+		var target *TargetConfig
+		if targetName != "" {
+			target = goal.GetTarget(targetName)
+		}
+
+		if target != nil {
+			layers = append(layers, target.Properties)
+		}
+
+		layers = append(layers, goal.Properties)
+	}
+
+	if plugin != nil {
+		layers = append(layers, plugin.Properties)
+	}
+
+	layers = append(layers, c.Properties)
 
 	return storage.Layers(layers...)
 }
 
+// GetTarget returns the TargetConfig for the given target name.
 func (g *GoalConfig) GetTarget(targetName string) *TargetConfig {
 	for i := range g.Targets {
 		if g.Targets[i].Name == targetName {
@@ -227,6 +332,7 @@ func (g *GoalConfig) GetTarget(targetName string) *TargetConfig {
 	return nil
 }
 
+// GetTarget returns the TargetConfig for the given target name.
 func (t *TaskConfig) GetTarget(targetName string) *TargetConfig {
 	for i := range t.Targets {
 		if t.Targets[i].Name == targetName {
