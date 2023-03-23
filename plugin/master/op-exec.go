9c9b9963535bbb43a725117bd4cd9c2cb7156728
Sterling Hanenkamp <sterling@hanenkamp.com>
2023-03-23 17:37:11 -0500

    godoc and break up code to make it easier to read

--- plugin/master/op-exec.go
+++ plugin/master/op-exec.go
@@ -8,13 +8,22 @@ import (
 	"github.com/zostay/zedpm/plugin"
 )
 
+// OperationExecutor is the abstraction that abstracts away the differences
+// between the different types of operations that are executed in the course of
+// executing an operation.
 type OperationExecutor interface {
 	Execute(context.Context) error
 }
 
+// OperationFunc is a more abstract version of plugin.OperationFunc.
 type OperationFunc func(plugin.Task, context.Context) error
+
+// PrepareFunc is an abstract version of plugin.Task operation functuons.
 type PrepareFunc func(plugin.Task, context.Context) (plugin.Operations, error)
 
+// SimpleExecutor is an executor that can execute one of the simple operations
+// on a task. As of this writing, this includes the setup, check, finish, and
+// teardown operations.
 type SimpleExecutor struct {
 	stageName string
 	exec      *InterfaceExecutor
@@ -22,6 +31,8 @@ type SimpleExecutor struct {
 	run       OperationFunc
 }
 
+// Execute runs all the configured operation stage for all tasks concurrently,
+// collects the errors, and returns them.
 func (s *SimpleExecutor) Execute(
 	ctx context.Context,
 ) error {
@@ -44,6 +55,9 @@ func (s *SimpleExecutor) Execute(
 	)
 }
 
+// CompletionExecutor executes the Complete operation that must run when a task
+// is complete. This runs Complete for all tasks concurrently, collects the
+// errors, and returns them.
 type CompletionExecutor struct {
 	exec  *InterfaceExecutor
 	tasks []plugin.TaskDescription
@@ -79,6 +93,13 @@ func (c *CompletionExecutor) Execute(
 	)
 }
 
+// StagedExecutor handles the primary staged phases: begin, run, and end. This
+// works by pulling all the prioritized operations and then executing each group
+// in priority order. Operations with the same priority will be executed
+// concurrently. Otherwise, operations are ordered by their plugin.Ordering,
+// from lowest numbered ordering to highest. If an error happens at any point,
+// the errors are returned and the operation terminates immediately without
+// continuing on to any remaining operations.
 type StagedExecutor struct {
 	stageName string
 	exec      *InterfaceExecutor
@@ -86,15 +107,17 @@ type StagedExecutor struct {
 	prepare   PrepareFunc
 }
 
-func (s *StagedExecutor) Execute(
-	ctx context.Context,
-) error {
-	type opInfo struct {
-		op       plugin.Operation
-		task     plugin.Task
-		taskName string
-	}
+type opInfo struct {
+	op       plugin.Operation
+	task     plugin.Task
+	taskName string
+}
 
+// prepareTasks returns a slice of prepared plugin.Task objects in the same
+// order as the plugin.TaskDescription objects in the tasks field.
+func (s *StagedExecutor) prepareTasks(
+	ctx context.Context,
+) ([]plugin.Task, error) {
 	tasks := make([]plugin.Task, len(s.tasks))
 	for _, taskDesc := range s.tasks {
 		taskName := taskDesc.Name()
@@ -103,12 +126,21 @@ func (s *StagedExecutor) Execute(
 			err = fmt.Errorf("failed to prepare task %q: %w", taskName, err)
 			s.exec.tryCancel(ctx, taskName, task, s.stageName)
 			s.exec.logFail(ctx, taskName, s.stageName, err)
-			return err
+			return nil, err
 		}
 
 		tasks = append(tasks, task)
 	}
+	return tasks, nil
+}
 
+// collateTasks returns the collected operations gathered together in execution
+// order with operations that need to be executed concurrently being grouped
+// together.
+func (s *StagedExecutor) collateTasks(
+	ctx context.Context,
+	tasks []plugin.Task,
+) (map[plugin.Ordering][]opInfo, error) {
 	ops := make(map[plugin.Ordering][]opInfo, 100)
 	for i, task := range tasks {
 		taskName := s.tasks[i].Name()
@@ -117,7 +149,7 @@ func (s *StagedExecutor) Execute(
 			err = fmt.Errorf("failed to prepare task %q: %w", taskName, err)
 			s.exec.tryCancel(ctx, taskName, task, s.stageName)
 			s.exec.logFail(ctx, taskName, s.stageName, err)
-			return err
+			return nil, err
 		}
 
 		for _, thisOp := range theseOps {
@@ -133,6 +165,14 @@ func (s *StagedExecutor) Execute(
 		}
 	}
 
+	return ops, nil
+}
+
+// executeOperations executes all the operations, each in their groups.
+func (s *StagedExecutor) executeOperations(
+	ctx context.Context,
+	ops map[plugin.Ordering][]opInfo,
+) error {
 	for i := plugin.Ordering(0); i < 100; i++ {
 		if stageOps, opsExist := ops[i]; opsExist {
 			err := RunTasksAndAccumulateErrors[int, opInfo](ctx,
@@ -158,4 +198,22 @@ func (s *StagedExecutor) Execute(
 	}
 
 	return nil
+}
+
+// Execute builds a plan for executing a staged operation and then executes that
+// plan. Any errors that occur along the way are returned.
+func (s *StagedExecutor) Execute(
+	ctx context.Context,
+) error {
+	tasks, err := s.prepareTasks(ctx)
+	if err != nil {
+		return err
+	}
+
+	ops, err := s.collateTasks(ctx, tasks)
+	if err != nil {
+		return err
+	}
+
+	return s.executeOperations(ctx, ops)
 }
