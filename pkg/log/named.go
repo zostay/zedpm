e17d651b93d45269ec3a1e1095986893358f7a26
Sterling Hanenkamp <sterling@speakeasyapi.dev>
2023-06-02 23:43:43 -0500

    Fix extra param problems with Smprintf

--- pkg/log/named.go
+++ pkg/log/named.go
@@ -3,54 +3,52 @@ package log
 import (
 	"fmt"
 	"strings"
+
+	strings2 "github.com/zostay/go-std/strings"
 )
 
 func Smprintf(f string, args ...any) string {
-	names, values := makeNames(args)
-	f = namedToPrintf(f, names)
+	used := findUsedNames(f)
+	values := makeValues(used, args)
+	f = namedToPrintf(f, used)
 	return fmt.Sprintf(f, values...)
 }
 
-func makeNames(args []any) (map[string]int, []any) {
-	names := make(map[string]int, len(args)/2)
-	values := make([]any, 0, len(args)/2)
-	for i := 0; i < len(args); i += 2 {
-		if str, isStr := args[i].(string); isStr && i+1 < len(args) {
-			names[str] = len(values) + 1
-			values = append(values, args[i+1])
-		}
-	}
-	return names, values
+type usedNames struct {
+	name string
+	s, e int
 }
 
-func namedToPrintf(f string, names map[string]int) string {
+func findUsedNames(f string) []usedNames {
 	rem := f
-	outFmt := &strings.Builder{}
+	consumed := 0
+	used := make([]usedNames, 0, 4)
 	for {
 		// Look for %v substrings
 		i := strings.IndexByte(rem, '%')
 		if i < 0 {
-			outFmt.WriteString(rem)
-			return outFmt.String()
+			return used
 		}
 
 		// Consume and ignore anything prior to the %v substring
-		outFmt.WriteString(rem[:i])
+		consumed += i
 		rem = rem[i:]
 
 		// Copy %% through...
 		if rem[1] == '%' {
-			outFmt.WriteString(rem[:2])
+			consumed += 2
 			rem = rem[:2]
 			continue
 		}
 
 		// Consume whatever is next until we reach [
 		startBracket := -1
+		contextStartBracket := -1
 		moreWork := -1
 		for i = 1; i < len(rem); i++ {
 			if rem[i] == '[' {
 				startBracket = i
+				contextStartBracket = consumed + i + 1
 				break
 			}
 
@@ -62,22 +60,23 @@ func namedToPrintf(f string, names map[string]int) string {
 
 		// We ran into a % before a [, so we will ignore this interpolation
 		if moreWork >= 0 {
-			outFmt.WriteString(rem[:moreWork])
+			consumed += moreWork
 			rem = rem[moreWork:]
 			continue
 		}
 
 		// We didn't find a bracket or a %, so this string is done
 		if startBracket < 0 {
-			outFmt.WriteString(rem)
-			return outFmt.String()
+			return used
 		}
 
 		// We found the [ we're looking for, now find the ]
 		stopBracket := -1
+		contextStopBracket := -1
 		for i = startBracket + 1; i < len(rem); i++ {
 			if rem[i] == ']' {
 				stopBracket = i
+				contextStopBracket = consumed + i
 				break
 			}
 
@@ -89,27 +88,60 @@ func namedToPrintf(f string, names map[string]int) string {
 
 		// We ran into a % before a ], so we will ignore this interpolation
 		if moreWork >= 0 {
-			outFmt.WriteString(rem[:moreWork])
+			consumed += moreWork
 			rem = rem[moreWork:]
 			continue
 		}
 
 		// We didn't find a bracket or a %, so this string is done
 		if stopBracket < 0 {
-			outFmt.WriteString(rem)
-			return outFmt.String()
+			return used
 		}
 
 		// We found both brackets, see if we can replace the contents with an index
-		if index, hasIndex := names[rem[startBracket+1:stopBracket]]; hasIndex {
-			outFmt.WriteString(rem[:startBracket+1])
-			_, _ = fmt.Fprintf(outFmt, "%d", index)
-			rem = rem[stopBracket:]
-			continue
-		}
+		name := rem[startBracket+1 : stopBracket]
+		used = append(used, usedNames{
+			name: name,
+			s:    contextStartBracket,
+			e:    contextStopBracket,
+		})
 
 		// If we're still here, we didn't find the name, so consume as-is and move on
-		outFmt.WriteString(rem[:stopBracket+1])
+		consumed += stopBracket + 1
 		rem = rem[stopBracket+1:]
 	}
+}
+
+func findNamed(name string, args []any) any {
+	for i := 0; i < len(args); i += 2 {
+		if str, isStr := args[i].(string); isStr && str == name && i+1 < len(args) {
+			return args[i+1]
+		}
+	}
+	return nil
+}
+
+func makeValues(used []usedNames, args []any) []any {
+	values := make([]any, len(used))
+	for i, u := range used {
+		values[i] = findNamed(u.name, args)
+	}
+	return values
+}
+
+func namedToPrintf(f string, used []usedNames) string {
+	bf := strings2.Reverse(f)
+	outFmt := &strings.Builder{}
+	from := 0
+	for i := len(used) - 1; i >= 0; i-- {
+		u := used[i]
+		e := len(f) - u.e
+
+		outFmt.WriteString(bf[from:e])
+		fmt.Fprintf(outFmt, "%d", i+1)
+
+		from = len(f) - u.s
+	}
+	outFmt.WriteString(bf[from:])
+	return strings2.Reverse(outFmt.String())
 }
--- pkg/log/named_test.go
+++ pkg/log/named_test.go
@@ -11,11 +11,23 @@ func TestSmprintf(t *testing.T) {
 	assert.Equal(t, "Test #42", str)
 
 	str = Smprintf("Test #%[answer]d", "answer")
-	assert.Equal(t, "Test #%!d(BADINDEX)", str)
+	assert.Equal(t, "Test #%!d(<nil>)", str)
 
 	str = Smprintf("Test #%[answer]d", []byte("answer"), 42)
-	assert.Equal(t, "Test #%!d(BADINDEX)", str)
+	assert.Equal(t, "Test #%!d(<nil>)", str)
 
-	str = Smprintf("%[a]d + %[b]d = %[c]d", "a", 2, "b", 2, "c", 4)
-	assert.Equal(t, "2 + 2 = 4", str)
+	str = Smprintf("%[a]d + %[b]d = %[c]d", "a", 1, "b", 2, "c", 3)
+	assert.Equal(t, "1 + 2 = 3", str)
+
+	str = Smprintf("%[c]d + %[b]d = %[a]d", "a", 1, "b", 2, "c", 3)
+	assert.Equal(t, "3 + 2 = 1", str)
+
+	str = Smprintf("%[c]d + %[c]d = %[c]d", "a", 1, "b", 2, "c", 3)
+	assert.Equal(t, "3 + 3 = 3", str)
+
+	str = Smprintf("%[c]d + %[b]d = %[c]d", "a", 1, "b", 2, "c", 3)
+	assert.Equal(t, "3 + 2 = 3", str)
+
+	str = Smprintf("%[c]d + %[d]d = %[c]d", "a", 1, "b", 2, "c", 3)
+	assert.Equal(t, "3 + %!d(<nil>) = 3", str)
 }
